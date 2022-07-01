dbd54d68929d7522cd6dd42d895c1a3d071b9b20
AIchrak <ichrak.aissia@outlook.com>
2022-07-01 09:59:24 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Converter</title>
+    <link rel="stylesheet" href="style.css"> 
+</head>
+  <body>
+    <h1>Currency Converter:</h1>
+  	<?php 
+    
+    ?>
+    
+  </body>
+</html>
